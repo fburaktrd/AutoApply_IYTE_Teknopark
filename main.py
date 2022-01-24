9c7a5d855662cfc8549e5a76e0c106ab796dfc7a
fburaktrd <fburakturedi@hotmail.com>
2022-01-24 03:36:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import requests
 import bs4
 from lets_mail import EmailSender
 
-content = requests.get("https://teknoparkizmir.com.tr/tr/firma-listesi/").text
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-
-soup = bs4.BeautifulSoup(content,"html.parser")
-
-companies = []
 
 
 def showCompanyInfos(company) -> None:
@@ -19,6 +12,7 @@ def showCompanyInfos(company) -> None:
 def filterCompany(companies,keywords) -> list:
     
     filteredCompanies = []
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     
     for keyword in keywords:
         for company in companies:
@@ -52,83 +46,94 @@ def filterCompany(companies,keywords) -> list:
     return filteredCompanies
 
 
-for comp in soup.find_all("div",{'class':'col-md-12 firmaListe holder'}): 
-    
-    company = dict()
+def getCompanies() -> list:
+    content = requests.get("https://teknoparkizmir.com.tr/tr/firma-listesi/").text
 
-    company['name'] = comp.find('h3',{'class':'title line'}).text 
-    
-    company['professions'] = comp.find('span',{'style':'padding-left:0px;font-size: 14px;'}).text.strip()
-    
-    company['informations'] = {"address":comp.find('div',{'class':'firmaAdres'}),
-                                "tel":comp.find('div',{'class':'tel'}),
-                                "web":comp.find('div',{'class':'web'}),
-                                "email":comp.find('div',{'class':'eposta'})}
 
-    for k,v in company['informations'].items():
+    soup = bs4.BeautifulSoup(content,"html.parser")
+
+    companies = []
+
+    for comp in soup.find_all("div",{'class':'col-md-12 firmaListe holder'}): 
         
-        try:
+        company = dict()
+
+        company['name'] = comp.find('h3',{'class':'title line'}).text 
+        
+        company['professions'] = comp.find('span',{'style':'padding-left:0px;font-size: 14px;'}).text.strip()
+        
+        company['informations'] = {"address":comp.find('div',{'class':'firmaAdres'}),
+                                    "tel":comp.find('div',{'class':'tel'}),
+                                    "web":comp.find('div',{'class':'web'}),
+                                    "email":comp.find('div',{'class':'eposta'})}
+
+        for k,v in company['informations'].items():
             
-            company['informations'][k] = v.text.strip()
+            try:
+                
+                company['informations'][k] = v.text.strip()
+            
+            except:
+
+                company['informations'][k] = 'No information found'
         
-        except:
-
-            company['informations'][k] = 'No information found'
+        companies.append(company)
     
-    companies.append(company)
+    return companies
+
+
+def main():
+    user_keywords = []
 
 
 
-user_keywords = []
+    while True:
+        
+        keyword = input("Please enter your keywords: ")# To end input session just press enter.
+        
+        if keyword == "":break
 
+        user_keywords.append(keyword)
 
+    print("Here is the companies that I found for you !")
 
-while True:
-    
-    keyword = input("Please enter your keywords: ")
-    
-    if keyword == "":break
+    companies = filterCompany(getCompanies(),user_keywords)
 
-    user_keywords.append(keyword)
-
-print("Here is the companies that I found for you !")
-
-companies = filterCompany(companies,user_keywords)
-
-for company in companies:
-    
-    print("\n\n"+"*-----------------------*"*5+"\n\n")
-    
-    showCompanyInfos(company)
-
-print(f"There are {len(companies)} companies for your taste !")
-
-
-
-
-
-sender = EmailSender("Your email","Your password")
-
-sender.session_login()# we logged in.
-
-with open("mail.txt","r") as file:
-    
-    text = "".join(file.readlines())
-
-with open("sent.txt","r+") as file:
-    
-    sent = {e_mail.strip("\n") for e_mail in file.readlines()}
-    
     for company in companies:
         
-        if company["informations"]["email"] not in sent:
+        print("\n\n"+"*-----------------------*"*5+"\n\n")
+        
+        showCompanyInfos(company)
 
-            sender.sendEmail(company["informations"]["email"],"Staj Başvurusu",text,["your_cv.pdf"])
+    print(f"There are {len(companies)} companies for your taste !")
+
+    sender = EmailSender("Your email","Your password")
+
+    sender.session_login()# we logged in.
+
+    with open("mail.txt","r") as file:
+        
+        text = "".join(file.readlines())
+
+    with open("sent.txt","r+") as file:
+        
+        sent = {e_mail.strip("\n") for e_mail in file.readlines()}
+        
+        for company in companies:
             
-            file.write(company["informations"]["email"]+"\n") # We write the emails in a text file because you might use the bot by searching diffrent keywords after the first run.
-                                                            # And the company might have other keywords that you inputed. We do not want to send email to same company again.
-                     
-sender.killSession()# At the end we end the session.
+            if company["informations"]["email"] not in sent:
+
+                sender.sendEmail(company["informations"]["email"],"Staj Başvurusu",text,["your_cv.pdf"])
+                
+                file.write(company["informations"]["email"]+"\n") # We write the emails in a text file because you might use the bot by searching diffrent keywords after the first run.
+                                                                # And the company might have other keywords that you inputed. We do not want to send email to same company again.
+                        
+    sender.killSession()# At the end we end the session.
+
+
+main()
+
+    
 
 
 
